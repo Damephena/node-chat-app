5f2f3d0c59e1d93832c6133087e7960a10443ae0
Ifenna O <okoyeifenna24@gmail.com>
2019-02-23 20:53:12 +0100

    Update for heroku in package.json

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 // for specifying relative/absoulte path using Node.js built-in module
 const path = require('path');
+// In order to use Socket.io, we need to configure http ourselves.
+const http = require('http');
 
 // Express route handlers
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+
+// Create a server ourselves(although already used behind the scene for Express).
+var server = http.createServer(app);
 
 // Heroku ready 
 const port = process.env.PORT || 3000;
@@ -15,6 +22,6 @@ const port = process.env.PORT || 3000;
 // Express static middleware configuration
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`***Started up at ${port}`);
 });
