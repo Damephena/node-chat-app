d8a22a9a6d339a75860a01dbf05b3f0d85e5bc10
Ifenna O <okoyeifenna24@gmail.com>
2019-03-09 08:13:14 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -35,11 +35,13 @@ socket.on('newLocationMessage', (message) => {
 jQuery('#message-form').on('submit', (e) => {
 	e.preventDefault(); //to override the 'refresh page' process
 
+	var messageTextbox = jQuery('[name=message]');
+
 	socket.emit('createMessage', {
 		from: 'User',
 		text: jQuery('[name=message]').val()
 	}, () => {
-
+		messageTextbox.val(''); // to clear input box after message is sent
 	});
 });
 
@@ -49,12 +51,16 @@ locationButton.on('click', () => {
 		return alert('Geolocation not supported by your browser.');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition((position) => {
+		locationButton.removeAttr('disabled').text('Send location'); 
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, () => {
+		locationButton.removeAttr('disabled').text('Send location'); 
 		alert('Unable to fetch location');
 	});
 });
